416b46c9d55d3d56f135c369b4d1ff68b4a6e7e8
Peter Chen <p.chen@tcu.edu>
2024-10-25 12:33:50 -0500

    Initialization of TankGame
    Initialization of TankGame

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.tankgame {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.tankgame to javafx.fxml;
+    exports com.example.tankgame;
+}
