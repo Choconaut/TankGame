b3695215859decc8a4d7af7f8f39c75ceb1417c8
Peter Chen <p.chen@tcu.edu>
2024-12-03 01:28:39 -0600

    Update TankApplication.java
    Added comment to TankApplication

--- src/main/java/com/example/tankgame/TankApplication.java
+++ src/main/java/com/example/tankgame/TankApplication.java
@@ -1,5 +1,9 @@
 package com.example.tankgame;
 
+/**
+ *  This class is the entry point of the application. It is used because
+ *  build does not like extends Application. (According to a video)
+ */
 public class TankApplication {
     public static void main(String[] args) {
         TankApp.main(args);
